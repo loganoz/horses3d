6729ffdfb087714ffe0ef5255e66bfcd7676ef61
David Kopriva <kopriva@math.fsu.edu>
2016-01-28 16:05:33 -0500

    Update the divergence tests to use the control variables dictionary.

--- Tests/Components/Gradients/Setup.f90
+++ Tests/Components/Gradients/Setup.f90
@@ -7,11 +7,50 @@
 !
 !////////////////////////////////////////////////////////////////////////
 !
+      Module mainKeywordsModule
+         IMPLICIT NONE 
+         INTEGER, PARAMETER :: KEYWORD_LENGTH = 132
+         CHARACTER(LEN=KEYWORD_LENGTH), PARAMETER :: machNumberKey           = "mach number"
+         CHARACTER(LEN=KEYWORD_LENGTH), PARAMETER :: reynoldsNumberKey       = "reynolds number"
+         CHARACTER(LEN=KEYWORD_LENGTH), PARAMETER :: aoaThetaKey             = "aoa theta"
+         CHARACTER(LEN=KEYWORD_LENGTH), PARAMETER :: aoaPhiKey               = "aoa phi"
+         CHARACTER(LEN=KEYWORD_LENGTH), PARAMETER :: flowIsNavierStokesKey   = "flowisnavierstokes"
+         CHARACTER(LEN=KEYWORD_LENGTH), PARAMETER :: polynomialOrderKey      = "polynomial order"
+         CHARACTER(LEN=KEYWORD_LENGTH), PARAMETER :: cflKey                  = "cfl"
+         CHARACTER(LEN=KEYWORD_LENGTH), PARAMETER :: meshFileNameKey         = "mesh file name"
+         CHARACTER(LEN=KEYWORD_LENGTH), PARAMETER :: restartKey              = "restart"
+         CHARACTER(LEN=KEYWORD_LENGTH), PARAMETER :: restartFileNameKey      = "restart file name"
+         CHARACTER(LEN=KEYWORD_LENGTH), PARAMETER :: numberOfTimeStepsKey    = "number of time steps"
+         CHARACTER(LEN=KEYWORD_LENGTH), PARAMETER :: outputIntervalKey       = "output interval"
+         CHARACTER(LEN=KEYWORD_LENGTH), PARAMETER :: convergenceToleranceKey = "convergence tolerance"
+         CHARACTER(LEN=KEYWORD_LENGTH), PARAMETER :: numberOfPlotPointsKey   = "number of plot points"
+         CHARACTER(LEN=KEYWORD_LENGTH), PARAMETER :: numberOfBoundariesKey   = "number of boundaries"
+         CHARACTER(LEN=KEYWORD_LENGTH), PARAMETER :: plotFileNameKey         = "plot file name"
+         CHARACTER(LEN=KEYWORD_LENGTH), DIMENSION(15) :: mainKeywords =  [machNumberKey,           &
+                                                                          reynoldsNumberKey,       &
+                                                                          aoaThetaKey,             &
+                                                                          aoaPhiKey,               &
+                                                                          flowIsNavierStokesKey,   &
+                                                                          polynomialOrderKey,      &
+                                                                          cflKey,                  &
+                                                                          meshFileNameKey,         &
+                                                                          restartKey,              &
+                                                                          restartFileNameKey,      &
+                                                                          numberOfTimeStepsKey,    &
+                                                                          outputIntervalKey,       &
+                                                                          convergenceToleranceKey, &
+                                                                          numberOfPlotPointsKey,   &
+                                                                          plotFileNameKey]
+      END MODULE mainKeywordsModule
+!
+!////////////////////////////////////////////////////////////////////////
+!
       MODULE setupModule
-      USE ControlVariablesModule
+      USE FTValueDictionaryClass
       USE DGSEMClass
+      USE mainKeywordsModule
       
-      TYPE( NSLiteControlVariables )  :: controlVariables
+      TYPE( FTValueDictionary )       :: controlVariables
       TYPE(DGsem)                     :: sem
       INTEGER                         :: testFileCount = 1
       CHARACTER(LEN=132), ALLOCATABLE :: meshFileNames(:)
@@ -36,18 +75,19 @@
 !     ---------------
 !
       CALL constructSharedBCModule
+      CALL controlVariables % initWithSize(16)
       CALL ReadInputFile( controlVariables )
-      CALL ConstructPhysicsStorage( controlVariables % mach,               &
-                                    controlVariables % RE,                 &
-                                    0.72_RP,                               &
-                                    controlVariables % flowIsNavierStokes )
+      CALL ConstructPhysicsStorage( controlVariables % doublePrecisionValueForKey(machNumberKey),     &
+                                    controlVariables % doublePrecisionValueForKey(reynoldsNumberKey), &
+                                    0.72_RP,                                                          &
+                                    controlVariables % logicalValueForKey(flowIsNavierStokesKey) )
 !
 !     --------------------------
 !     Get the file names to test
 !     --------------------------
 !
       fUnit = UnusedUnit()
-      OPEN(UNIT = fUnit, FILE = controlVariables % inputFileName)
+      OPEN(UNIT = fUnit, FILE = controlVariables % stringValueForKey(meshFileNameKey,KEYWORD_LENGTH))
          READ(fUnit,*) numberOfMeshfiles
          ALLOCATE(meshFileNames(numberOfMeshfiles))
          DO j = 1, numberOfMeshfiles
@@ -78,7 +118,7 @@
 !        ----------------
 !
          CALL ConstructDGSem(self              = sem, &
-                             polynomialOrder   = controlVariables % polynomialOrder,&
+                             polynomialOrder   = controlVariables % integerValueForKey(polynomialOrderKey),&
                              meshFileName      = meshFileName,  &
                              externalState     = externalBoundaryState,  &
                              externalGradients = externalGradientState,  &
