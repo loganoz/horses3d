acbbbedd77dd3a476fe425391c23e24537af3731
Juan Manzanero <juan.manzanero@upm.es>
2017-09-21 17:46:35 +0200

    Forgot to stash the TGV ProblemFile

---
+++ Solver/test/NavierStokes/TaylorGreen/SETUP/ProblemFile.f90
@@ -0,0 +1,306 @@
+!
+!////////////////////////////////////////////////////////////////////////
+!
+!      ProblemFile.f90
+!      Created: June 26, 2015 at 8:47 AM 
+!      By: David Kopriva  
+!
+!      The Problem File contains user defined procedures
+!      that are used to "personalize" i.e. define a specific
+!      problem to be solved. These procedures include initial conditions,
+!      exact solutions (e.g. for tests), etc. and allow modifications 
+!      without having to modify the main code.
+!
+!      The procedures, *even if empty* that must be defined are
+!
+!      UserDefinedSetUp
+!      UserDefinedInitialCondition(sem)
+!      UserDefinedPeriodicOperation(sem)
+!      UserDefinedFinalize(sem)
+!      UserDefinedTermination
+!
+!//////////////////////////////////////////////////////////////////////// 
+! 
+         SUBROUTINE UserDefinedStartup
+!
+!        --------------------------------
+!        Called before any other routines
+!        --------------------------------
+!
+            IMPLICIT NONE  
+         END SUBROUTINE UserDefinedStartup
+!
+!//////////////////////////////////////////////////////////////////////// 
+! 
+         SUBROUTINE UserDefinedFinalSetup(sem , thermodynamics_, &
+                                                 dimensionless_, &
+                                                     refValues_ )
+!
+!           ----------------------------------------------------------------------
+!           Called after the mesh is read in to allow mesh related initializations
+!           or memory allocations.
+!           ----------------------------------------------------------------------
+!
+            USE DGSEMClass
+            use PhysicsStorage
+            IMPLICIT NONE
+            CLASS(DGSem)             :: sem
+            type(Thermodynamics_t), intent(in)  :: thermodynamics_
+            type(Dimensionless_t),  intent(in)  :: dimensionless_
+            type(RefValues_t),      intent(in)  :: refValues_
+         END SUBROUTINE UserDefinedFinalSetup
+!
+!//////////////////////////////////////////////////////////////////////// 
+! 
+         SUBROUTINE UserDefinedInitialCondition(sem, thermodynamics_, &
+                                                      dimensionless_, &
+                                                          refValues_  )
+!
+!           ------------------------------------------------
+!           Called to set the initial condition for the flow
+!              - By default it sets an uniform initial
+!                 condition.
+!           ------------------------------------------------
+!
+            USE SMConstants
+            use PhysicsStorage
+            use DGSEMClass
+            implicit none
+            class(DGSEM)                        :: sem
+            type(Thermodynamics_t), intent(in)  :: thermodynamics_
+            type(Dimensionless_t),  intent(in)  :: dimensionless_
+            type(RefValues_t),      intent(in)  :: refValues_
+!
+!           ---------------
+!           Local variables
+!           ---------------
+!
+            REAL(KIND=RP) :: x(3)        
+            INTEGER       :: i, j, k, eID
+            REAL(KIND=RP) :: rho , u , v , w , p
+            REAL(KIND=RP) :: L, u_0, rho_0, p_0
+            integer       :: Nx, Ny, Nz
+            
+            L     = 1.0_RP
+            u_0   = 1.0_RP
+            rho_0 = 1.0_RP 
+            p_0   = 100.0_RP
+
+            associate( gamma => thermodynamics_ % gamma ) 
+            DO eID = 1, SIZE(sem % mesh % elements)
+               Nx = sem % mesh % elements(eID) % Nxyz(1)
+               Ny = sem % mesh % elements(eID) % Nxyz(2)
+               Nz = sem % mesh % elements(eID) % Nxyz(3)
+
+               DO k = 0, Nz
+                  DO j = 0, Ny
+                     DO i = 0, Nx 
+
+                         x = sem % mesh % elements(eID) % geom % x(:,i,j,k)
+                       
+                         rho = rho_0
+                         u   =  u_0 * sin(x(1)/L) * cos(x(2)/L) * cos(x(3)/L) 
+                         v   = -u_0 * cos(x(1)/L) * sin(x(2)/L) * cos(x(3)/L)
+                         w   =  0.0_RP
+                         p   =   p_0 + rho_0 / 16.0_RP * (                          &
+                               cos(2.0_RP*x(1)/L)*cos(2.0_RP*x(3)/L) +                  &
+                               2.0_RP*cos(2.0_RP*x(2)/L) + 2.0_RP*cos(2.0_RP*x(1)/L) +  &
+                               cos(2.0_RP*x(2)/L)*cos(2.0_RP*x(3)/L)                    &
+                               )
+
+                         sem % mesh % elements(eID) % Q(i,j,k,1) = rho
+                         sem % mesh % elements(eID) % Q(i,j,k,2) = rho*u
+                         sem % mesh % elements(eID) % Q(i,j,k,3) = rho*v
+                         sem % mesh % elements(eID) % Q(i,j,k,4) = rho*w
+                         sem % mesh % elements(eID) % Q(i,j,k,5) = p / (gamma - 1.0_RP) + 0.5_RP * rho * (u*u + v*v + w*w)
+
+                     END DO
+                  END DO
+               END DO 
+               
+            END DO 
+            end associate
+            
+            
+         END SUBROUTINE UserDefinedInitialCondition
+
+         subroutine UserDefinedState1(x, t, nHat, Q, thermodynamics_, dimensionless_, refValues_)
+!
+!           -------------------------------------------------
+!           Used to define an user defined boundary condition
+!           -------------------------------------------------
+!
+            use SMConstants
+            use PhysicsStorage
+            implicit none
+            real(kind=RP), intent(in)     :: x(NDIM)
+            real(kind=RP), intent(in)     :: t
+            real(kind=RP), intent(in)     :: nHat(NDIM)
+            real(kind=RP), intent(inout)  :: Q(N_EQN)
+            type(Thermodynamics_t),    intent(in)  :: thermodynamics_
+            type(Dimensionless_t),     intent(in)  :: dimensionless_
+            type(RefValues_t),         intent(in)  :: refValues_
+         end subroutine UserDefinedState1
+
+         subroutine UserDefinedNeumann(x, t, nHat, U_x, U_y, U_z)
+!
+!           --------------------------------------------------------
+!           Used to define a Neumann user defined boundary condition
+!           --------------------------------------------------------
+!
+            use SMConstants
+            use PhysicsStorage
+            implicit none
+            real(kind=RP), intent(in)     :: x(NDIM)
+            real(kind=RP), intent(in)     :: t
+            real(kind=RP), intent(in)     :: nHat(NDIM)
+            real(kind=RP), intent(inout)  :: U_x(N_GRAD_EQN)
+            real(kind=RP), intent(inout)  :: U_y(N_GRAD_EQN)
+            real(kind=RP), intent(inout)  :: U_z(N_GRAD_EQN)
+         end subroutine UserDefinedNeumann
+
+!
+!//////////////////////////////////////////////////////////////////////// 
+! 
+         SUBROUTINE UserDefinedPeriodicOperation(sem, time)
+!
+!           ----------------------------------------------------------
+!           Called at the output interval to allow periodic operations
+!           to be performed
+!           ----------------------------------------------------------
+!
+            USE DGSEMClass
+            IMPLICIT NONE
+            CLASS(DGSem)  :: sem
+            REAL(KIND=RP) :: time
+!
+!           ---------------
+!           Local variables
+!           ---------------
+!
+            LOGICAL, SAVE       :: created = .FALSE.
+            REAL(KIND=RP)       :: k_energy = 0.0_RP
+            INTEGER             :: fUnit
+            INTEGER, SAVE       :: restartfile = 0
+            INTEGER             :: restartUnit
+            CHARACTER(LEN=100)  :: fileName
+            interface
+               function computeKineticEnergy(sem) result(val)
+                  use DGSEMClass
+                  implicit none
+                  class(DGSem)        :: sem
+                  real(kind=RP)       :: val
+               end function computeKineticEnergy
+            end interface
+
+
+            fUnit = 101
+
+            IF (.NOT. created) THEN
+                OPEN(fUnit, file = "./RESULTS/kinetic_energy.dat", action = "WRITE" , status = "UNKNOWN" )
+                created = .TRUE.
+            ELSE
+                OPEN(fUnit, file = "./RESULTS/kinetic_energy.dat", action = "WRITE" , status = "OLD" , access = "APPEND")
+            END IF
+!           -------------------------------
+!           Compute the flow kinetic energy
+!           -------------------------------
+            k_energy = computeKineticEnergy(sem)
+            WRITE(fUnit , '(E24.16,E24.16)') time , k_energy
+
+            CLOSE(fUnit)
+            
+         END SUBROUTINE UserDefinedPeriodicOperation
+
+         FUNCTION computeKineticEnergy(sem) result(val)
+            USE DGSEMClass
+            IMPLICIT NONE
+            CLASS(DGSem)        :: sem
+            REAL(kind=RP)       :: val
+            
+            REAL(kind=RP)       :: rho, u , v , w
+            INTEGER             :: eID , i , j , k
+            integer             :: Nx, Ny, Nz
+
+!           Initialization
+            val = 0.0_RP
+
+            DO eID = 1 , SIZE(sem % mesh % elements)
+               Nx = sem % mesh % elements(eID) % Nxyz(1)
+               Ny = sem % mesh % elements(eID) % Nxyz(2)
+               Nz = sem % mesh % elements(eID) % Nxyz(3)
+
+                DO k = 0 , Nz
+                    DO j = 0 , Ny
+                        DO i = 0 , Nx
+
+                            rho = sem % mesh % elements(eID) % Q(i,j,k,1)
+                            u = sem % mesh % elements(eID) % Q(i,j,k,2) / rho
+                            v = sem % mesh % elements(eID) % Q(i,j,k,3) / rho
+                            w = sem % mesh % elements(eID) % Q(i,j,k,4) / rho
+
+                            ASSOCIATE(wx => sem % spA(Nx,Ny,Nz) % wx , &
+                                      wy => sem % spA(Nx,Ny,Nz) % wy , &
+                                      wz => sem % spA(Nx,Ny,Nz) % wz   )
+                              val = val + 0.5_RP*rho*(u*u+v*v+w*w)*wx(i)*wy(j)*wz(k)*      &
+                                    sem % mesh % elements (eID) % geom % jacobian (i,j,k)
+                            END ASSOCIATE
+                             
+                        END DO
+                    END DO
+                END DO
+            END DO
+
+            val = val / (2*acos(-1.d0))**3
+
+         END FUNCTION computeKineticEnergy 
+!
+!//////////////////////////////////////////////////////////////////////// 
+! 
+         SUBROUTINE UserDefinedFinalize(sem, time, thermodynamics_, &
+                                                    dimensionless_, &
+                                                        refValues_   )
+!
+!           --------------------------------------------------------
+!           Called after the solution computed to allow, for example
+!           error tests to be performed
+!           --------------------------------------------------------
+!
+            use FTAssertions
+            USE DGSEMClass
+            use PhysicsStorage
+            IMPLICIT NONE
+            CLASS(DGSem)  :: sem
+            REAL(KIND=RP) :: time
+            type(Thermodynamics_t),    intent(in)  :: thermodynamics_
+            type(Dimensionless_t),     intent(in)  :: dimensionless_
+            type(RefValues_t),         intent(in)  :: refValues_
+!
+!           ---------------
+!           Local variables
+!           ---------------
+!
+            CHARACTER(LEN=29)                  :: testName           = "Taylor-Green vortex"
+            REAL(KIND=RP)                      :: maxError
+            REAL(KIND=RP), ALLOCATABLE         :: QExpected(:,:,:,:)
+            INTEGER                            :: eID
+            INTEGER                            :: i, j, k, N
+            TYPE(FTAssertionsManager), POINTER :: sharedManager
+            LOGICAL                            :: success
+
+            WRITE(6,*) "This test case has no expected solution yet."
+
+         END SUBROUTINE UserDefinedFinalize
+!
+!//////////////////////////////////////////////////////////////////////// 
+! 
+      SUBROUTINE UserDefinedTermination
+!
+!        -----------------------------------------------
+!        Called at the the end of the main driver after 
+!        everything else is done.
+!        -----------------------------------------------
+!
+         IMPLICIT NONE  
+      END SUBROUTINE UserDefinedTermination
+      
