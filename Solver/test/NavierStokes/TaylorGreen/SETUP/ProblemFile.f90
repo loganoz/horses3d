26527a712b399c0baa7f5f9905f61092f5d4cd02
Juan Manzanero <juan.manzanero@upm.es>
2017-10-26 17:33:43 +0200

    Added source terms to the NS equation

--- Solver/test/NavierStokes/TaylorGreen/SETUP/ProblemFile.f90
+++ Solver/test/NavierStokes/TaylorGreen/SETUP/ProblemFile.f90
@@ -162,6 +162,41 @@
 !
 !//////////////////////////////////////////////////////////////////////// 
 ! 
+         subroutine UserDefinedSourceTerm(mesh, time, thermodynamics_, dimensionless_, refValues_)
+!
+!           --------------------------------------------
+!           Called to apply source terms to the equation
+!           --------------------------------------------
+!
+            USE HexMeshClass
+            use PhysicsStorage
+            IMPLICIT NONE
+            CLASS(HexMesh)                        :: mesh
+            REAL(KIND=RP)                         :: time
+            type(Thermodynamics_t),    intent(in) :: thermodynamics_
+            type(Dimensionless_t),     intent(in) :: dimensionless_
+            type(RefValues_t),         intent(in) :: refValues_
+!
+!           ---------------
+!           Local variables
+!           ---------------
+!
+            integer  :: i, j, k, eID
+!
+!           Usage example (by default no source terms are added)
+!           ----------------------------------------------------
+!           do eID = 1, mesh % no_of_elements
+!              associate ( e => mesh % elements(eID) )
+!              do k = 0, e % Nxyz(3)   ; do j = 0, e % Nxyz(2) ; do i = 0, e % Nxyz(1)
+!                 e % QDot(:,i,j,k) = e % QDot(:,i,j,k) + Source(:)
+!              end do                  ; end do                ; end do
+!           end do
+   
+         end subroutine UserDefinedSourceTerm
+!
+!//////////////////////////////////////////////////////////////////////// 
+! 
+
          SUBROUTINE UserDefinedPeriodicOperation(mesh, time)
 !
 !           ----------------------------------------------------------
