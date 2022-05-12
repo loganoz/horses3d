c13b58a3b8e893683f9d76d4968f939a153c789b
Andrés Mateo Gabín <andres.mgabin@upm.es>
2022-05-12 17:10:52 +0200

    Fix modal SC sensor

--- Solver/src/libs/discretization/SCsensorClass.f90
+++ Solver/src/libs/discretization/SCsensorClass.f90
@@ -552,18 +552,19 @@ module SCsensorClass
 !     ---------------
       integer               :: eID
       integer               :: i, j, k, r
-      integer               :: Nx, Ny, Nz
+      integer               :: maxNx, maxNy, maxNz
       real(RP), allocatable :: sVar(:,:,:)
       real(RP), allocatable :: sVarMod(:,:,:)
+      real(RP), allocatable :: Lwx(:), Lwy(:), Lwz(:)
       real(RP)              :: num, den
 
 
       ! Only allocate once
-      Nx = maxval(sem % mesh % Nx)
-      Ny = maxval(sem % mesh % Ny)
-      Nz = maxval(sem % mesh % Nz)
-      allocate(sVar(Nx,Ny,Nz))
-      allocate(sVarMod(Nx,Ny,Nz))
+      maxNx = maxval(sem % mesh % Nx)
+      maxNy = maxval(sem % mesh % Ny)
+      maxNz = maxval(sem % mesh % Nz)
+      allocate(sVar(maxNx,maxNy,maxNz))
+      allocate(sVarMod(maxNx,maxNy,maxNz))
 
 !$omp parallel do schedule(runtime) private(eID, sVar, sVarMod)
       do eID = 1, sem % mesh % no_of_elements
@@ -610,14 +611,42 @@ module SCsensorClass
 !                     Representing them as cubes, this subspace corresponds to the outer shell of
 !                     V(Nx,Ny,Nz), thus the terms "faces", "edges" and "corner"
 !        --------------------------------------------------------------------------------------------
-         num = sum(sVarMod(Nx,0:Ny-1,0:Nz-1)**2) &  ! +X face
-             + sum(sVarMod(0:Nx-1,Ny,0:Nz-1)**2) &  ! +Y face
-             + sum(sVarMod(0:Nx-1,0:Ny-1,Nz)**2) &  ! +Z face
-             + sum(sVarMod(0:Nx-1,Ny,Nz))        &  ! +X edge
-             + sum(sVarMod(Nx,0:Ny-1,Nz))        &  ! +Y edge
-             + sum(sVarMod(Nx,Ny,0:Nz-1))        &  ! +Z edge
-             + sVarMod(Nx,Ny,Nz)**2                 ! XYZ corner
-         den = sum(sVarMod**2)
+         Lwx = NodalStorage(Nx) % Lw
+         Lwy = NodalStorage(Ny) % Lw
+         Lwz = NodalStorage(Nz) % Lw
+
+         ! Corner (Nx, Ny, Nz)
+         num = sVarMod(Nx,Ny,Nz)**2 * Lwx(Nx) * Lwy(Ny) * Lwz(Nz)
+
+         ! Edges
+         do i = 0, Nx-1  ! +X edge
+            num = num + sVarMod(i,Ny,Nz)**2 * Lwx(i) * Lwy(Ny) * Lwz(Nz)
+         end do
+         do j = 0, Ny-1  ! +Y edge
+            num = num + sVarMod(Nx,j,Nz)**2 * Lwx(Nx) * Lwy(j) * Lwz(Nz)
+         end do
+         do k = 0, Nz-1  ! +Z edge
+            num = num + sVarMod(Nx,Ny,k)**2 * Lwx(Nx) * Lwy(Ny) * Lwz(k)
+         end do
+
+         ! Faces
+         do k = 0, Nz-1 ; do j = 0, Ny-1  ! +X face
+            num = num + sVarMod(Nx,j,k)**2 * Lwx(Nx) * Lwy(j) * Lwz(k)
+         end do         ; end do
+         do k = 0, Nz-1 ; do i = 0, Nx-1  ! +Y face
+            num = num + sVarMod(i,Ny,k)**2 * Lwx(i) * Lwy(Ny) * Lwz(k)
+         end do         ; end do
+         do j = 0, Ny-1 ; do i = 0, Nx-1  ! +Z face
+            num = num + sVarMod(i,j,Nz)**2 * Lwx(i) * Lwy(j) * Lwz(Nz)
+         end do         ; end do
+
+         ! Total sum
+         den = 0.0_RP
+         do k = 0, Nz ; do j = 0, Ny ; do i = 0, Nx
+            den = den + sVarMod(i,j,k)**2 * Lwx(i) * Lwy(j) * Lwz(k)
+         end do       ; end do       ; end do
+
+         ! Sensor value as the ratio of num / den
          e % storage % sensor = log10( num / den )
 
          end associate
