fa0f0053e3f561b3d2704038badc09bc7901bdb0
Andrés Mateo Gabín <andres.mgabin@upm.es>
2022-09-28 17:59:38 -0400

    Add MPI utilities: MPI_OpAll, MPI_MinMax

--- Solver/src/libs/mpiutils/MPI_Utilities.f90
+++ Solver/src/libs/mpiutils/MPI_Utilities.f90
@@ -4,9 +4,19 @@ module MPI_Utilities
 #ifdef _HAS_MPI_
    use mpi
 #endif
-   
+
    private
-   public infNorm, L2Norm, MPI_SumAll
+   public infNorm, L2Norm, MPI_OpAll, MPI_SumAll, MPI_MinMax
+
+   interface MPI_OpAll
+      module procedure MPI_OpAll_int
+      module procedure MPI_OpAll_real
+   end interface MPI_OpAll
+
+   interface MPI_SumAll
+      module procedure MPI_SumAll_int
+      module procedure MPI_SumAll_real
+   end interface MPI_SumAll
 !
 !  ========
    contains
@@ -21,7 +31,7 @@ module MPI_Utilities
       real(kind=RP) :: xloc ! local x
       integer       :: ierr
       !-------------------------------------------------
-      
+
       if (MPI_Process % doMPIAction) then
          xloc = sum(x**2)
 #ifdef _HAS_MPI_
@@ -31,9 +41,9 @@ module MPI_Utilities
       else
          norm = sqrt(sum(x*x))
       end if
-         
+
    end function L2Norm
-   
+
    function infNorm(x) result(norm)
       implicit none
       !-arguments---------------------------------------
@@ -43,31 +53,143 @@ module MPI_Utilities
       real(kind=RP) :: xloc ! local x
       integer       :: ierr
       !-------------------------------------------------
-      
+
       norm = maxval(abs(x))
-      
+
       if (MPI_Process % doMPIAction) then
          xloc = norm
 #ifdef _HAS_MPI_
          call mpi_allreduce(xloc, norm, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD, ierr)
 #endif
       end if
-         
+
    end function infNorm
-   
-   subroutine MPI_SumAll(x)
+
+   subroutine MPI_OpAll_int(x, op)
       implicit none
       !-arguments---------------------------------------
-      real(kind=RP), intent(inout) :: x
+      integer, intent(inout) :: x(..)
+      integer, intent(in)    :: op
       !-local-variables---------------------------------
-      real(kind=RP) :: xloc ! local x
+      integer :: xloc(size(x)) ! local x
+      integer :: ierr
+      !-------------------------------------------------
+
+#ifdef _HAS_MPI_
+      select rank(x)
+      rank(0)
+         xloc = x
+         call mpi_allreduce(xloc, x, 1, MPI_INTEGER, op, MPI_COMM_WORLD, ierr)
+      rank(1)
+         xloc = x
+         call mpi_allreduce(xloc, x, size(x), MPI_INTEGER, op, MPI_COMM_WORLD, ierr)
+      rank(2)
+         xloc = pack(x, .true.)
+         call mpi_allreduce(xloc, x, size(x), MPI_INTEGER, op, MPI_COMM_WORLD, ierr)
+      rank(3)
+         xloc = pack(x, .true.)
+         call mpi_allreduce(xloc, x, size(x), MPI_INTEGER, op, MPI_COMM_WORLD, ierr)
+      end select
+#endif
+   end subroutine MPI_OpAll_int
+
+   subroutine MPI_OpAll_real(x, op)
+      implicit none
+      !-arguments---------------------------------------
+      real(kind=RP), intent(inout) :: x(..)
+      integer,       intent(in)    :: op
+      !-local-variables---------------------------------
+      real(kind=RP) :: xloc(size(x)) ! local x
       integer       :: ierr
       !-------------------------------------------------
-      
+
 #ifdef _HAS_MPI_
-      xloc = x
-      call mpi_allreduce(xloc, x, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD, ierr)
+      select rank(x)
+      rank(0)
+         xloc = x
+         call mpi_allreduce(xloc, x, 1, MPI_DOUBLE, op, MPI_COMM_WORLD, ierr)
+      rank(1)
+         xloc = x
+         call mpi_allreduce(xloc, x, size(x), MPI_DOUBLE, op, MPI_COMM_WORLD, ierr)
+      rank(2)
+         xloc = pack(x, .true.)
+         call mpi_allreduce(xloc, x, size(x), MPI_DOUBLE, op, MPI_COMM_WORLD, ierr)
+      rank(3)
+         xloc = pack(x, .true.)
+         call mpi_allreduce(xloc, x, size(x), MPI_DOUBLE, op, MPI_COMM_WORLD, ierr)
+      end select
 #endif
-   end subroutine MPI_SumAll
-   
+   end subroutine MPI_OpAll_real
+
+   subroutine MPI_SumAll_int(x)
+      implicit none
+      !-arguments---------------------------------------
+      integer, intent(inout) :: x(..)
+      !-------------------------------------------------
+
+#ifdef _HAS_MPI_
+      call MPI_OpAll(x, MPI_SUM)
+#endif
+   end subroutine MPI_SumAll_int
+
+   subroutine MPI_SumAll_real(x)
+      implicit none
+      !-arguments---------------------------------------
+      real(kind=RP), intent(inout) :: x(..)
+      !-------------------------------------------------
+
+#ifdef _HAS_MPI_
+      call MPI_OpAll(x, MPI_SUM)
+#endif
+   end subroutine MPI_SumAll_real
+
+   subroutine MPI_MinMax(minimum, maximum)
+      implicit none
+      !-arguments---------------------------------------
+      real(kind=RP), intent(inout) :: minimum(..), maximum(..)
+      !-local-variables---------------------------------
+      real(kind=RP), allocatable :: lminmax_in(:), lminmax_out(:)
+      integer                    :: nmin, nmax, n
+      integer                    :: ierr
+      !-------------------------------------------------
+
+#ifdef _HAS_MPI_
+      select rank(minimum)
+      rank(0)
+         select rank(maximum)
+         rank(0)
+
+            lminmax_in = [-minimum, maximum]
+            allocate(lminmax_out(2))
+
+            call mpi_allreduce(lminmax_in, lminmax_out, 2, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD, ierr)
+
+            minimum = -lminmax_out(1)
+            maximum = lminmax_out(2)
+
+         end select
+
+      rank(1)
+         select rank(maximum)
+         rank(1)
+
+            nmin = size(minimum)
+            nmax = size(maximum)
+            n = nmin + nmax
+
+            lminmax_in = [-minimum, maximum]
+            allocate(lminmax_out(n))
+
+            call mpi_allreduce(lminmax_in, lminmax_out, n, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD, ierr)
+
+            minimum = -lminmax_out(1:nmin)
+            maximum = lminmax_out(nmin+1:n)
+
+         end select
+
+      end select
+
+#endif
+   end subroutine MPI_MinMax
+
 end module MPI_Utilities
