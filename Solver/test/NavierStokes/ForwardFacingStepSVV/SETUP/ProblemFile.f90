340a5eb4618ed92cd4c45fd684af05e952a3a0a3
Andrés Mateo Gabín <andres.mgabin@upm.es>
2022-10-11 00:13:43 -0400

    Fix FFS test

--- Solver/test/NavierStokes/ForwardFacingStepSVV/SETUP/ProblemFile.f90
+++ Solver/test/NavierStokes/ForwardFacingStepSVV/SETUP/ProblemFile.f90
@@ -527,8 +527,8 @@ end module ProblemFileFunctions
             type(FTAssertionsManager), pointer :: sharedManager
             real(kind=RP), parameter           :: SVVdiss = 7.56510e-8_RP
             real(kind=RP), parameter           :: res(5) = [ 3590.0433_RP, &
-                                                             831.1562_RP, &
-                                                             176.0218_RP, &
+                                                             838.7658_RP, &
+                                                             185.8130_RP, &
                                                              0.0_RP, &
                                                              3018.6708_RP  ]
 
