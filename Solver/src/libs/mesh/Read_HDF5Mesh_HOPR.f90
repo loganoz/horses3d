5f9564081d9af8cdf0c9a01f659dda2ad3267d9d
Hatem Kessasra <kesshatem@gmail.com>
2023-04-13 12:02:46 +0200

    Update Read_HDF5Mesh_HOPR.f90

--- Solver/src/libs/mesh/Read_HDF5Mesh_HOPR.f90
+++ Solver/src/libs/mesh/Read_HDF5Mesh_HOPR.f90
@@ -451,6 +451,7 @@ contains
          self % nonconforming= .TRUE.
          CALL ConstructFaces( self, success,numberOfElements, HorsesMortars)    
        end if 
+
 !
 !
 !     -------------------------
