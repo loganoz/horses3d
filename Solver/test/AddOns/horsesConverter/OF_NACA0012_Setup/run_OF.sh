9b18efd8f1608fe80f9b18a3fd15244e5e515203
Himpu Marbona <himpu.marbona@upm.es>
2023-04-21 12:46:25 +0200

    Test Folder Addons for horsesConverter

--- Solver/test/AddOns/horsesConverter/OF_NACA0012_Setup/run_OF.sh
+++ Solver/test/AddOns/horsesConverter/OF_NACA0012_Setup/run_OF.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 renumberMesh -overwrite > log.renumberMesh &
 wait
 checkMesh > log.checkMesh &
@@ -10,6 +9,10 @@ wait
 mpirun -np 8 rhoPimpleFoam -parallel > log.rhoPimpleFoam_1 &
 wait
 reconstructPar -latestTime > log.reconstructPar &
+wait
+foamToVTK -fields "(U p T rho)" -ascii -latestTime > log.foamToVTK_1 &
+wait
+
 
 
 
