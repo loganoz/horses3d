da2582e282ebe7f1750d599a481d07d02742ef18
Hatem Kessasra <kesshatem@gmail.com>
2023-05-15 23:21:31 +0200

    Mortar update

--- Solver/src/libs/mesh/HexMesh.f90
+++ Solver/src/libs/mesh/HexMesh.f90
@@ -3193,6 +3193,7 @@ slavecoord:             DO l = 1, 4
          character(len=*),    intent(in)        :: name
          logical,             intent(in)        :: saveGradients
          logical, optional,   intent(in)        :: saveSensor_
+         logical, optional,   intent(in)        :: saveLES_
 !
 !        ---------------
 !        Local variables
@@ -3202,7 +3203,7 @@ slavecoord:             DO l = 1, 4
          integer(kind=AddrInt)            :: pos
          real(kind=RP)                    :: refs(NO_OF_SAVED_REFS)
          real(kind=RP), allocatable       :: Q(:,:,:,:)
-         logical                          :: saveSensor
+         logical                          :: saveSensor, saveLES
 #if (!defined(NAVIERSTOKES))
          logical                          :: computeGradients = .true.
 #endif
@@ -3233,6 +3234,11 @@ slavecoord:             DO l = 1, 4
             saveSensor = saveSensor_
          else
             saveSensor = .false.
+         end if
+         if (present(saveLES_)) then
+            saveLES = saveLES_
+         else
+            saveLES = .false.
          end if
 
          if (saveGradients .and. computeGradients) then
@@ -3313,6 +3319,17 @@ slavecoord:             DO l = 1, 4
             if (saveSensor) then
                write(fid) e % storage % sensor
             end if
+
+          if (saveLES) then
+#if defined(NAVIERSTOKES) && (!(SPALARTALMARAS))
+               allocate(Q(1,0:e % Nxyz(1), 0:e % Nxyz(2), 0:e % Nxyz(3)))
+               Q(1,:,:,:) = e % storage % mu_NS(1,:,:,:) ! total viscosity = mu + mu_sgs
+               write(fid) Q
+               Q(1,:,:,:) = e % storage % mu_turb_NS(:,:,:) !mu_sgs
+               write(fid) Q
+               deallocate(Q)
+#endif
+          end if 
 
             end associate
          end do
