702f41fba9d909d6951bed178c27554480fe60cb
Andrés Mateo Gabín <andres.mgabin@upm.es>
2023-06-27 13:02:58 +0200

    Add sensor based on the gradient of the Mach

--- Solver/src/libs/discretization/ShockCapturingKeywords.f90
+++ Solver/src/libs/discretization/ShockCapturingKeywords.f90
@@ -70,43 +70,45 @@ module ShockCapturingKeywords
 !
 !  Sensed variables
 !  ----------------
-   character(len=*), parameter :: SC_RHO_VAL      = "rho"
-   character(len=*), parameter :: SC_RHOU_VAL     = "rhou"
-   character(len=*), parameter :: SC_RHOV_VAL     = "rhov"
-   character(len=*), parameter :: SC_RHOW_VAL     = "rhow"
-   character(len=*), parameter :: SC_RHOE_VAL     = "rhoe"
-   character(len=*), parameter :: SC_U_VAL        = "u"
-   character(len=*), parameter :: SC_V_VAL        = "v"
-   character(len=*), parameter :: SC_W_VAL        = "w"
-   character(len=*), parameter :: SC_P_VAL        = "p"
-   character(len=*), parameter :: SC_ENT_VAL      = "entropy"
-   character(len=*), parameter :: SC_RHOP_VAL     = "rhop"
-   character(len=*), parameter :: SC_GRAD_RHO_VAL = "grad rho"
-   character(len=*), parameter :: SC_GRAD_P_VAL   = "grad p"
-   character(len=*), parameter :: SC_GRAD_1_VAL   = "grad 1"
-   character(len=*), parameter :: SC_GRAD_5_VAL   = "grad 5"
-   character(len=*), parameter :: SC_DIV_V_VAL    = "div v"
-   character(len=*), parameter :: SC_MACH_V_VAL   = "mach"
-   character(len=*), parameter :: SC_MACH_N_V_VAL = "mach perp"
+   character(len=*), parameter :: SC_RHO_VAL       = "rho"
+   character(len=*), parameter :: SC_RHOU_VAL      = "rhou"
+   character(len=*), parameter :: SC_RHOV_VAL      = "rhov"
+   character(len=*), parameter :: SC_RHOW_VAL      = "rhow"
+   character(len=*), parameter :: SC_RHOE_VAL      = "rhoe"
+   character(len=*), parameter :: SC_U_VAL         = "u"
+   character(len=*), parameter :: SC_V_VAL         = "v"
+   character(len=*), parameter :: SC_W_VAL         = "w"
+   character(len=*), parameter :: SC_P_VAL         = "p"
+   character(len=*), parameter :: SC_ENT_VAL       = "entropy"
+   character(len=*), parameter :: SC_RHOP_VAL      = "rhop"
+   character(len=*), parameter :: SC_GRAD_RHO_VAL  = "grad rho"
+   character(len=*), parameter :: SC_GRAD_P_VAL    = "grad p"
+   character(len=*), parameter :: SC_GRAD_1_VAL    = "grad 1"
+   character(len=*), parameter :: SC_GRAD_5_VAL    = "grad 5"
+   character(len=*), parameter :: SC_DIV_V_VAL     = "div v"
+   character(len=*), parameter :: SC_MACH_V_VAL    = "mach"
+   character(len=*), parameter :: SC_MACH_N_V_VAL  = "mach perp"
+   character(len=*), parameter :: SC_GRAD_MACH_VAL = "grad mach"
 
-   integer, parameter :: SC_RHO_ID      = 1
-   integer, parameter :: SC_RHOU_ID     = 2
-   integer, parameter :: SC_RHOV_ID     = 3
-   integer, parameter :: SC_RHOW_ID     = 4
-   integer, parameter :: SC_RHOE_ID     = 5
-   integer, parameter :: SC_U_ID        = 6
-   integer, parameter :: SC_V_ID        = 7
-   integer, parameter :: SC_W_ID        = 8
-   integer, parameter :: SC_P_ID        = 9
-   integer, parameter :: SC_ENT_ID      = 10
-   integer, parameter :: SC_RHOP_ID     = 11
-   integer, parameter :: SC_GRAD_RHO_ID = 12
-   integer, parameter :: SC_GRAD_P_ID   = 13
-   integer, parameter :: SC_GRAD_1_ID   = 14
-   integer, parameter :: SC_GRAD_5_ID   = 15
-   integer, parameter :: SC_DIV_V_ID    = 16
-   integer, parameter :: SC_MACH_V_ID   = 17
-   integer, parameter :: SC_MACH_N_V_ID = 18
+   integer, parameter :: SC_RHO_ID       = 1
+   integer, parameter :: SC_RHOU_ID      = 2
+   integer, parameter :: SC_RHOV_ID      = 3
+   integer, parameter :: SC_RHOW_ID      = 4
+   integer, parameter :: SC_RHOE_ID      = 5
+   integer, parameter :: SC_U_ID         = 6
+   integer, parameter :: SC_V_ID         = 7
+   integer, parameter :: SC_W_ID         = 8
+   integer, parameter :: SC_P_ID         = 9
+   integer, parameter :: SC_ENT_ID       = 10
+   integer, parameter :: SC_RHOP_ID      = 11
+   integer, parameter :: SC_GRAD_RHO_ID  = 12
+   integer, parameter :: SC_GRAD_P_ID    = 13
+   integer, parameter :: SC_GRAD_1_ID    = 14
+   integer, parameter :: SC_GRAD_5_ID    = 15
+   integer, parameter :: SC_DIV_V_ID     = 16
+   integer, parameter :: SC_MACH_V_ID    = 17
+   integer, parameter :: SC_MACH_N_V_ID  = 18
+   integer, parameter :: SC_GRAD_MACH_ID = 19
 !
 !  Derivative types
 !  ----------------
