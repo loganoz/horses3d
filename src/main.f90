b157f5a59c4f6868572c126dd5c8aae25d8f8a50
HatemKsr <108345480+HatemKsr@users.noreply.github.com>
2024-06-29 18:19:07 +0000

    Deploying to gh-pages from @ loganoz/horses3d@8a61f6c7131ea26cfd612bf41a8098c74ed52507 🚀

--- src/main.f90
+++ src/main.f90
@@ -1,63 +1,51 @@
-!
-!//////////////////////////////////////////////////////
-!
-!  Program to merge statistics files
-!  Usage:
-!
-!  horses.mergeStats *.hsol --initial-iteration=INTEGER --file-name=CHARACTER
-!
-!  NOTES:
-!  *  Only usable with statistics files that are obtained with the "reset interval" keyword
-!     and/or with individual consecutive simulations 
-!  *  Only constant time-stepping is supported
-!  *  Dynamic p-adaptation is currently not supported
-!
-program MergeStatistics
+program ExtractGeometry
    use SMConstants
-   use getTaskModule       , only: getTask
-   use Headers  !           , only: Main_Header, Section_Header
-   use StatsAveragingModule, only: Initialize_StatsAveragingModule, Finalize_StatsAveragingModule, PerformAveraging
-   use MPI_Process_Info    , only: MPI_Process
+   use Headers
+   use NodalStorageClass
+   use HexMeshClass
+   use ConstructMeshAndSpectralBasis_MOD
+   use FTValueDictionaryClass
+   use getInputData_MOD
+   use GeometryClass
+   use MPI_Process_Info
    implicit none
+   character(len=LINE_LENGTH)      :: meshFile
+   character(len=LINE_LENGTH)      :: solutionFile
+   type(HexMesh)                   :: mesh
+   type(FTValueDictionary)         :: controlVariables
+   class(Geometry_t), pointer      :: geometry
 !
-!  -----------------
-!  Program variables
-!  -----------------
-!
-   integer                                 :: num_of_statFiles, initial_iteration
-   real(kind=RP), allocatable              :: weights(:)
-   character(len=LINE_LENGTH), allocatable :: fileNames(:)
-   character(len=LINE_LENGTH)              :: fileName
-!
-!  ------------------------
-!  Initializations
-!  ------------------------
-!
+!  Init MPI
+!  --------
    call MPI_Process % Init
-   
-   call Main_Header("HORSES Merge Statistics Utility",__DATE__,__TIME__)
-   
-   write(STD_OUT,'(/,/)')
-   call Section_Header("Job description")
-   write(STD_OUT,'(/,/)')
-   
-   call getTask(num_of_statFiles, fileNames, weights, initial_iteration, fileName)
-   
-   call Initialize_StatsAveragingModule(num_of_statFiles, fileNames)
-   
 !
-!  ------------------------
-!  Average statistics files
-!  ------------------------
+!  Display header
+!  --------------
+   call Main_Header("Extract geometry utility",__DATE__,__TIME__)
 !
-   call PerformAveraging(num_of_statFiles, fileNames, weights, fileName)
+!  Initialize control variables
+!  ----------------------------
+   call controlVariables % InitWithSize(16)
+   call getInputData(controlVariables)
 !
+!  Construct mesh and spectral basis
+!  ---------------------------------
+   call ConstructMeshAndSpectralBasis( controlVariables % StringValueForKey(meshFileKey, LINE_LENGTH), &
+                                       controlVariables % StringValueForKey(solutionFileKey, LINE_LENGTH), &
+                                       controlVariables % StringValueForKey(ControlFileKey, LINE_LENGTH), &
+                                       mesh, controlVariables)
+!
+!  Create geometry
+!  ---------------
+   geometry => Construct(mesh, NodalStorage, controlVariables)
+!
+!  Get new points coordinates and elements
+!  ---------------------------------------
+   call geometry % Compute(mesh, NodalStorage)
+!
+!  Close MPI
 !  ---------
-!  Finish up
-!  ---------
-!
-   call Finalize_StatsAveragingModule
-   
    call MPI_Process % Close
-   
-end program MergeStatistics
+
+
+end program ExtractGeometry
