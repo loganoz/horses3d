f7884a0849516c08d1550e1bb3edc8bea5807883
HatemKsr <108345480+HatemKsr@users.noreply.github.com>
2024-06-29 18:34:22 +0000

    Deploying to gh-pages from @ loganoz/horses3d@8ea3d4a1ce1d53d29d7177f5dff1de3f6084c845 🚀

--- src/main.f90
+++ src/main.f90
@@ -1,51 +1,121 @@
-program ExtractGeometry
+program horses2plt
    use SMConstants
+   use getTask
+   use Mesh2PltModule
+   use Solution2PltModule
+   use Solution2VtkHdfModule
+   use Solution2FoamModule
+   use Stats2PltModule
+   use SolutionFile
+   use SharedSpectralBasis
    use Headers
-   use NodalStorageClass
-   use HexMeshClass
-   use ConstructMeshAndSpectralBasis_MOD
-   use FTValueDictionaryClass
-   use getInputData_MOD
-   use GeometryClass
    use MPI_Process_Info
    implicit none
-   character(len=LINE_LENGTH)      :: meshFile
-   character(len=LINE_LENGTH)      :: solutionFile
-   type(HexMesh)                   :: mesh
-   type(FTValueDictionary)         :: controlVariables
-   class(Geometry_t), pointer      :: geometry
-!
-!  Init MPI
-!  --------
+   integer                                 :: jobType
+   character(len=LINE_LENGTH)              :: meshName
+   integer                                 :: no_of_solutions
+   character(len=LINE_LENGTH), allocatable :: solutionNames(:)
+   integer, allocatable                    :: solutionTypes(:)
+   logical                                 :: fixedOrder
+   integer                                 :: Nout(3)
+   integer                                 :: basis
+   integer                                 :: mode
+   integer                                 :: iSol
+   logical                                 :: useCommandArgs
+   logical                                 :: oldStats
+   logical 								   :: writeMesh
+
    call MPI_Process % Init
+
+   call Main_Header("HORSES to TecPlot conversion utility",__DATE__,__TIME__)
 !
-!  Display header
-!  --------------
-   call Main_Header("Extract geometry utility",__DATE__,__TIME__)
+!  Get the job type
+!  ----------------
+   call getTaskType(jobType, meshName, no_of_solutions, solutionNames, solutionTypes, fixedOrder, Nout, basis, mode, useCommandArgs, oldStats, writeMesh)
 !
-!  Initialize control variables
-!  ----------------------------
-   call controlVariables % InitWithSize(16)
-   call getInputData(controlVariables)
+!  Construct Spectral basis
+!  ------------------------
+   call ConstructSpectralBasis
 !
-!  Construct mesh and spectral basis
+!  Perform the conversion to tecplot
 !  ---------------------------------
-   call ConstructMeshAndSpectralBasis( controlVariables % StringValueForKey(meshFileKey, LINE_LENGTH), &
-                                       controlVariables % StringValueForKey(solutionFileKey, LINE_LENGTH), &
-                                       controlVariables % StringValueForKey(ControlFileKey, LINE_LENGTH), &
-                                       mesh, controlVariables)
+   select case (jobType)
+   case (MESH_2_PLT)
+      write(STD_OUT,'(/,/)')
+      call Section_Header("Mesh conversion")
+      call Mesh2Plt(meshName)
+
+   case (SOLUTION_2_PLT)
+      do iSol = 1, no_of_solutions
+
+         select case (solutionTypes(iSol))
+         case ( SOLUTION_FILE, ZONE_SOLUTION_FILE, ZONE_SOLUTION_AND_DOT_FILE, SOLUTION_AND_SENSOR_FILE )
+            write(STD_OUT,'(/,/)')
+            call Section_Header("Solution conversion")
+            call Solution2Plt(meshName, solutionNames(iSol), fixedOrder, basis, Nout, mode)        
+
+         case ( SOLUTION_AND_GRADIENTS_FILE, SOLUTION_AND_GRADIENTS_AND_SENSOR_FILE)
+            write(STD_OUT,'(/,/)')
+            call Section_Header("Solution with gradients conversion")
+            call Solution2Plt(meshName, solutionNames(iSol), fixedOrder, basis, Nout, mode)        
+
+         case ( STATS_FILE )
+            write(STD_OUT,'(/,/)')
+            if (oldStats) then
+                call Section_Header("Statistics legacy file conversion")
+                call Stats2Plt(meshName, solutionNames(iSol), fixedOrder, basis, Nout)
+            else
+                call Section_Header("Statistics file conversion")
+                call Solution2Plt(meshName, solutionNames(iSol), fixedOrder, basis, Nout, mode)        
+            end if 
+
+         end select
+      end do
+
+   case (SOLUTION_2_VTKHDF)
+#ifdef HAS_HDF5
+      do iSol = 1, no_of_solutions
+
+         select case (solutionTypes(iSol))
+         case ( SOLUTION_FILE, ZONE_SOLUTION_FILE, ZONE_SOLUTION_AND_DOT_FILE, SOLUTION_AND_SENSOR_FILE )
+            write(STD_OUT,'(/,/)')
+            call Section_Header("Solution conversion")
+            call Solution2VtkHdf(meshName, solutionNames(iSol), Nout)
+
+         case ( SOLUTION_AND_GRADIENTS_FILE, SOLUTION_AND_GRADIENTS_AND_SENSOR_FILE)
+            write(STD_OUT,'(/,/)')
+            call Section_Header("Solution with gradients conversion")
+            call Solution2VtkHdf(meshName, solutionNames(iSol), Nout)
+
+         case ( STATS_FILE )
+            write(STD_OUT,'(/,/)')
+            if (oldStats) then
+                write(STD_OUT,'(A)') "Statistics legacy file conversion not supported for VTKHDF"
+                error stop
+            else
+               call Section_Header("Statistics file conversion")
+               call Solution2VtkHdf(meshName, solutionNames(iSol), Nout)
+            end if
+
+         end select
+      end do
+#else
+      write(STD_OUT, '(A)') "HDF5 support must be enabled to save to VTKHDF format"
+      error stop
+#endif
+
+   case (SOLUTION_2_FOAM)
+         call Solution2Foam (meshName, no_of_solutions, solutionNames, solutionTypes, Nout, writeMesh)  
+
+   case (UNKNOWN_JOB)
+      call exit(UNKNOWN_JOB)
+
+   end select
 !
-!  Create geometry
-!  ---------------
-   geometry => Construct(mesh, NodalStorage, controlVariables)
-!
-!  Get new points coordinates and elements
-!  ---------------------------------------
-   call geometry % Compute(mesh, NodalStorage)
-!
-!  Close MPI
-!  ---------
+!  Destruct Spectral basis
+!  -----------------------
+   call DestructSpectralBasis
+   
    call MPI_Process % Close
 
-
-end program ExtractGeometry
+end program horses2plt
