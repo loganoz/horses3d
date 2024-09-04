cb7a523f1adcecec29b857618290b5ed12f61371
Yu Zhang <1225839327@qq.com>
2024-09-04 18:35:12 +0200

    The LES-Wale model has been corrected.

--- Solver/src/libs/physics/common/LESModels.f90
+++ Solver/src/libs/physics/common/LESModels.f90
@@ -373,21 +373,24 @@ module LESModels
          real(kind=RP)  :: U_y(NDIM)
          real(kind=RP)  :: U_z(NDIM)
          integer        :: i,j
+         integer        :: k   ! The third index
          !-------------------------------------------------------
-         
          call getVelocityGradients  (Q,Q_x,Q_y,Q_z,U_x,U_y,U_z)
 
          gradV(1,:) = U_x(1:3)
          gradV(2,:) = U_y(1:3)
          gradV(3,:) = U_z(1:3)
- 
          do i = 1, 3 
             do j = 1, 3 
                S(i,j)      = 0.5_RP*(gradV(i,j)+gradV(j,i))
-               gradV2(i,j) = 0.5_RP*(gradV(i,j)*gradV(j,i))
+
+               gradV2(i,j) = 0
+               do k = 1,3
+                  gradV2(i,j) = gradV2(i,j) + gradV(i,k)*gradV(k,j) 
+               end do
+
             end do  
          end do  
-         
          divV2 = gradV2(1,1) + gradV2(2,2) + gradV2(3,3)
 
          normS =  sum(S*S)
@@ -402,18 +405,30 @@ module LESModels
          Sd(2,2) = Sd(2,2) - 1.0_RP / 3.0_RP * divV2
          Sd(3,3) = Sd(3,3) - 1.0_RP / 3.0_RP * divV2
 
+   !//////////////////////////////////////////////////////////////////////////////////////
+   !        These are the code after expanding the do loop, these codes equal to the above, but hard.
+      !   Sd(1,1) = 2.0_RP/3.0*gradV(1,1)**2 + 1.0_RP/3.0*gradV(1,2)*gradV(2,1) + 1.0_RP/3.0*gradV(1,3)*gradV(3,1) - 1.0_RP/3.0*gradV(2,2)**2 - 2.0_RP/3.0*gradV(2,3)*gradV(3,2) - 1.0_RP/3.0*gradV(3,3)**2  
+      !   Sd(1,2) = 0.5_RP*gradV(1,1)*gradV(1,2) + 0.5_RP*gradV(1,1)*gradV(2,1) + 0.5_RP*gradV(1,2)*gradV(2,2) + 0.5_RP*gradV(1,3)*gradV(3,2) + 0.5_RP*gradV(2,1)*gradV(2,2) + 0.5_RP*gradV(2,3)*gradV(3,1)  
+      !   Sd(1,3) = 0.5_RP*gradV(1,1)*gradV(1,3) + 0.5_RP*gradV(1,1)*gradV(3,1) + 0.5_RP*gradV(1,2)*gradV(2,3) + 0.5_RP*gradV(1,3)*gradV(3,3) + 0.5_RP*gradV(2,1)*gradV(3,2) + 0.5_RP*gradV(3,1)*gradV(3,3)  
+      !   Sd(2,1) = 0.5_RP*gradV(1,1)*gradV(1,2) + 0.5_RP*gradV(1,1)*gradV(2,1) + 0.5_RP*gradV(1,2)*gradV(2,2) + 0.5_RP*gradV(1,3)*gradV(3,2) + 0.5_RP*gradV(2,1)*gradV(2,2) + 0.5_RP*gradV(2,3)*gradV(3,1)  
+      !   Sd(2,2) = -1.0_RP/3.0*gradV(1,1)**2 + 1.0_RP/3.0*gradV(1,2)*gradV(2,1) - 2.0_RP/3.0*gradV(1,3)*gradV(3,1) + 2.0_RP/3.0*gradV(2,2)**2 + 1.0_RP/3.0*gradV(2,3)*gradV(3,2) - 1.0_RP/3.0*gradV(3,3)**2 
+      !   Sd(2,3) = 0.5_RP*gradV(1,2)*gradV(3,1) + 0.5_RP*gradV(1,3)*gradV(2,1) + 0.5_RP*gradV(2,2)*gradV(2,3) + 0.5_RP*gradV(2,2)*gradV(3,2) + 0.5_RP*gradV(2,3)*gradV(3,3) + 0.5_RP*gradV(3,2)*gradV(3,3)  
+      !   Sd(3,1) = 0.5_RP*gradV(1,1)*gradV(1,3) + 0.5_RP*gradV(1,1)*gradV(3,1) + 0.5_RP*gradV(1,2)*gradV(2,3) + 0.5_RP*gradV(1,3)*gradV(3,3) + 0.5_RP*gradV(2,1)*gradV(3,2) + 0.5_RP*gradV(3,1)*gradV(3,3)  
+      !   Sd(3,2) = 0.5_RP*gradV(1,2)*gradV(3,1) + 0.5_RP*gradV(1,3)*gradV(2,1) + 0.5_RP*gradV(2,2)*gradV(2,3) + 0.5_RP*gradV(2,2)*gradV(3,2) + 0.5_RP*gradV(2,3)*gradV(3,3) + 0.5_RP*gradV(3,2)*gradV(3,3)  
+      !   Sd(3,3) = -1.0_RP/3.0*gradV(1,1)**2 - 2.0_RP/3.0*gradV(1,2)*gradV(2,1) + 1.0_RP/3.0*gradV(1,3)*gradV(3,1) - 1.0_RP/3.0*gradV(2,2)**2 + 1.0_RP/3.0*gradV(2,3)*gradV(3,2) + 2.0_RP/3.0*gradV(3,3)**2
+   !//////////////////////////////////////////////////////////////////////////////////////
+
          normSd =  sum(Sd*Sd)
          LS = this % Cw * delta
 
-#if defined (NAVIERSTOKES) 
+   #if defined (NAVIERSTOKES)
          mu = Q(IRHO) * POW2(LS) * (normSd**(3.0_RP / 2.0_RP) / (normS**(5.0_RP / 2.0_RP)+normSd**(5.0_RP / 4.0_RP)))
-#endif
-#if defined (INCNS) 
+   #endif
+   #if defined (INCNS)
          mu = Q(INSRHO) * POW2(LS) * (normSd**(3.0_RP / 2.0_RP) / (normS**(5.0_RP / 2.0_RP)+normSd**(5.0_RP / 4.0_RP)))
-#endif
+   #endif
 
          if (normS<1.0e-8_RP .and. normSd<1.0e-8_RP) mu=0.0_RP
-         
       end subroutine WALE_ComputeViscosity
 
       subroutine WALE_Describe(self)
