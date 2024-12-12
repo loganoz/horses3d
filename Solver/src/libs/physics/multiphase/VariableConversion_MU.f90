7b85dd3691be078908a6558e9f83247dec026f43
Abbas Ballout <abb.ballout@gmail.com>
2024-12-12 15:17:08 +0100

    LES added to MU (untested)

--- Solver/src/libs/physics/multiphase/VariableConversion_MU.f90
+++ Solver/src/libs/physics/multiphase/VariableConversion_MU.f90
@@ -8,6 +8,7 @@ module VariableConversion_MU
    private
    public   mGradientVariables
    public   GetmTwoFluidsViscosity, GetmOneFluidViscosity
+   public   getVelocityGradients
 
    contains
 !
@@ -83,6 +84,32 @@ module VariableConversion_MU
          mu = dimensionless % mu(1) * cHat + dimensionless % mu(2) * (1.0_RP - cHat)
 
       end subroutine GetmTwoFluidsViscosity
+!
+! /////////////////////////////////////////////////////////////////////
+!
+      pure subroutine getVelocityGradients(Q,Q_x,Q_y,Q_z,dimensionless_,U_x,U_y,U_z)
+      implicit none
+      !-arguments---------------------------------------------------
+      real(kind=RP), intent(in)  :: Q(NCONS)
+      real(kind=RP), intent(in)  :: Q_x(NGRAD), Q_y(NGRAD), Q_z(NGRAD)
+      class(dimensionless_t),intent(in) :: dimensionless_ 
+      real(kind=RP), intent(out) :: U_x(NDIM), U_y(NDIM), U_z(NDIM)
+      !-local-variables---------------------------------------------
+      real(kind=RP) :: rho, invRho, invRho2, uDivRho(NDIM)
+      !-------------------------------------------------------------
+
+       rho = dimensionless_ % rho(1)*Q(IMC) + dimensionless_ % rho(2)*(1-Q(IMC))
+       invRho  = 1._RP / rho
+       invRho2 = invRho * invRho
+      
+       uDivRho = [Q(IMSQRHOU) , Q(IMSQRHOV) , Q(IMSQRHOW) ] * invRho2
+      
+       u_x = invRho * Q_x(IMSQRHOU:IMSQRHOW) - uDivRho * Q_x(IMC)*(dimensionless_ % rho(1) - dimensionless_ % rho(2))
+       u_y = invRho * Q_y(IMSQRHOU:IMSQRHOW) - uDivRho * Q_y(IMC)*(dimensionless_ % rho(1) - dimensionless_ % rho(2))
+       u_z = invRho * Q_z(IMSQRHOU:IMSQRHOW) - uDivRho * Q_z(IMC)*(dimensionless_ % rho(1) - dimensionless_ % rho(2))
+
+   end subroutine getVelocityGradients
+
 !
 ! /////////////////////////////////////////////////////////////////////
 !
