5c4cb7506c8a9806cb4ead5a4f333dff82a7e177
Oscar Marino <oscar.marino@upm.es>
2025-01-27 14:27:16 +0100

    also start from 0 in TimeDerivative_VolumetricContribution_Split

--- Solver/src/NavierStokesSolver/SpatialDiscretization.f90
+++ Solver/src/NavierStokesSolver/SpatialDiscretization.f90
@@ -1404,6 +1404,8 @@ module SpatialDiscretization
                      mesh % elements(eID) % storage % contravariantFlux(eq,i,j,k,IZ) = - Flux(eq,IX) * mesh % elements(eID) % geom % jGradZeta(IX,i,j,k)  &
                                                                                        - Flux(eq,IY) * mesh % elements(eID) % geom % jGradZeta(IY,i,j,k)  &
                                                                                        - Flux(eq,IZ) * mesh % elements(eID) % geom % jGradZeta(IZ,i,j,k)
+                     ! initialize to 0 to accumulate
+                     mesh % elements(eID) % storage % Qdot(eq,i,j,k,IZ)  = 0.0_RP
                   end do
             end do               ; end do                ; end do
 
